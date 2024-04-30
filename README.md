897e5be2a3feb4da9a751fb297223ca79392c786
Rambabu Kollabathula <rambabu.kollabathula@gmail.com>
2024-04-30 19:09:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# DataDrivenFrameworkBankApp" 
---
+++ src/test/java/com/w2a/base/TestBase.java
@@ -0,0 +1,218 @@
+package com.w2a.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.Status;
+import com.w2a.listeners.CustomListeners;
+import com.w2a.utilities.ExcelReader;
+import com.w2a.utilities.TestUtil;
+
+public class TestBase {
+	//again test2
+	/*
+	 * WebDriver - done Properties - done Logs - log4j jar, .log,
+	 * log4j.properties, Logger ExtentReports DB Excel Mail ReportNG,
+	 * ExtentReports Jenkins
+	 * 
+	 */
+
+	public static WebDriver driver;
+	public static Properties config = new Properties();
+	public static Properties OR = new Properties();
+	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger(TestBase.class);
+	public static ExcelReader excel = new ExcelReader(
+			System.getProperty("user.dir") + "\\src\\test\\resources\\excel\\testdata.xlsx");
+	public static WebDriverWait wait;
+
+	public static String browser;
+
+	@BeforeSuite
+	public void setUp() {
+
+		if (driver == null) {
+
+			PropertyConfigurator.configure("./src/test/resources/properties/log4j.properties");
+
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				config.load(fis);
+				log.debug("Config file loaded !!!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\OR.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				OR.load(fis);
+				log.debug("OR file loaded !!!");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		
+			if(System.getenv("browser")!=null && !System.getenv("browser").isEmpty()){
+				
+				browser = System.getenv("browser");
+			}else{
+				
+				browser = config.getProperty("browser");
+				
+			}
+			
+			config.setProperty("browser", browser);
+			
+			
+			System.out.println(config.getProperty("browser"));
+
+			if (config.getProperty("browser").equals("firefox")) {
+
+					driver = new FirefoxDriver();
+
+			} else if (config.getProperty("browser").equals("chrome")) {
+
+						driver = new ChromeDriver();
+				log.debug("Chrome Launched !!!");
+			} else if (config.getProperty("browser").equals("ie")) {
+
+				driver = new InternetExplorerDriver();
+
+			}
+
+			driver.get(config.getProperty("testsiteurl"));
+			log.debug("Navigated to : " + config.getProperty("testsiteurl"));
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.parseInt(config.getProperty("implicit.wait"))));
+			wait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(config.getProperty("implicit.wait"))));
+		}
+
+	}
+
+	public void click(String locator) {
+
+		if (locator.endsWith("_CSS")) {
+			driver.findElement(By.cssSelector(OR.getProperty(locator))).click();
+		} else if (locator.endsWith("_XPATH")) {
+			driver.findElement(By.xpath(OR.getProperty(locator))).click();
+		} else if (locator.endsWith("_ID")) {
+			driver.findElement(By.id(OR.getProperty(locator))).click();
+		}
+		CustomListeners.testReport.get().log(Status.INFO, "Clicking on : " + locator);
+	}
+
+	public void type(String locator, String value) {
+
+		if (locator.endsWith("_CSS")) {
+			driver.findElement(By.cssSelector(OR.getProperty(locator))).sendKeys(value);
+		} else if (locator.endsWith("_XPATH")) {
+			driver.findElement(By.xpath(OR.getProperty(locator))).sendKeys(value);
+		} else if (locator.endsWith("_ID")) {
+			driver.findElement(By.id(OR.getProperty(locator))).sendKeys(value);
+		}
+
+		CustomListeners.testReport.get().log(Status.INFO, "Typing in : " + locator + " entered value as " + value);
+
+	}
+	
+	static WebElement dropdown;
+
+	public void select(String locator, String value) {
+
+		if (locator.endsWith("_CSS")) {
+			dropdown = driver.findElement(By.cssSelector(OR.getProperty(locator)));
+		} else if (locator.endsWith("_XPATH")) {
+			dropdown = driver.findElement(By.xpath(OR.getProperty(locator)));
+		} else if (locator.endsWith("_ID")) {
+			dropdown = driver.findElement(By.id(OR.getProperty(locator)));
+		}
+		
+		Select select = new Select(dropdown);
+		select.selectByVisibleText(value);
+
+		CustomListeners.testReport.get().log(Status.INFO, "Selecting from dropdown : " + locator + " value as " + value);
+
+	}
+
+	public boolean isElementPresent(By by) {
+
+		try {
+
+			driver.findElement(by);
+			return true;
+
+		} catch (NoSuchElementException e) {
+
+			return false;
+
+		}
+
+	}
+
+	public static void verifyEquals(String expected, String actual) throws IOException {
+
+		try {
+
+			Assert.assertEquals(actual, expected);
+
+		} catch (Throwable t) {
+
+			TestUtil.captureScreenshot();
+			// ReportNG
+			Reporter.log("<br>" + "Verification failure : " + t.getMessage() + "<br>");
+			Reporter.log("<a target=\"_blank\" href=" + TestUtil.screenshotName + "><img src=" + TestUtil.screenshotName
+					+ " height=200 width=200></img></a>");
+			Reporter.log("<br>");
+			Reporter.log("<br>");
+			// Extent Reports
+			CustomListeners.testReport.get().log(Status.FAIL, " Verification failed with exception : " + t.getMessage());
+			//CustomListeners.testReport.get().log(Status.FAIL, CustomListeners.testReport.get().addScreenCaptureFromPath(TestUtil.screenshotName));
+
+		}
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		if (driver != null) {
+			driver.quit();
+		}
+
+		log.debug("test execution completed !!!");
+	}
+}
